d31ca7d7de201f67ce753ef32dcb811cc951b41e
Tomás Longobardi <tomaslongobardi@MacBook-Air-de-Tomas.local>
2021-05-23 17:21:14 -0300

    Context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,6 @@ import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 import Products from './Components/Product/Products';
 import PaymentConfirmation from './Components/Payment/Confirmation';
 import PaymentError from './Components/Payment/Error';
-import UsersList from './Components/UsersList';
-import CartList from './Components/CartList';
-
 import CartState from "./Context/Cart/CartState";
 
 function App() {
@@ -18,8 +15,6 @@ function App() {
           <Route exact path="/" component={() => <Products/> }/>
           <Route exact path="/payment_confirmation" component={() => <PaymentConfirmation/> }/>
           <Route exact path="/payment_error" component={() => <PaymentError/> }/>
-          <Route exact path="/users" component={() => <UsersList/> }/>
-          <Route exact path="/cart" component={() => <CartList/> }/>
         </Switch>
       </div>
     </BrowserRouter>
